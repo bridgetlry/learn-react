02fc3a81a8c26c868f8326239776d2b6df17fb11
Bridget Leary <beleary25@gmail.com>
2025-01-24 16:09:50 -0500

    fixed list_keys_ids

---
+++ src/pages/solncomps/list_keys_id.tsx
@@ -0,0 +1,32 @@
+export const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'szV5sdG'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'YfeOqp2'
+}];
+
+function getImageUrl(imageId: string) {
+  return "https://i.imgur.com/" + imageId + "s.jpg"
+}
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <h3>{person.name}</h3>
+      <img
+        src={getImageUrl(person.imageId)}
+      />
+      <ul>
+        <li>Profession: {person.profession}</li>
+        <li>Accomplishment: {person.accomplishment}</li>
+      </ul>
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
